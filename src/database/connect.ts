95522a750578aac966c2f30bc7217042e48619c0
Ivan Maltsev <i.malcev@mediapronet.ru>
2020-04-22 01:21:19 +0300

    remove cache

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,8 +1,5 @@
 import * as firebase from "firebase/app";
 
-// Add the Firebase products that you want to use
-// import "firebase/auth";
-// import "firebase/firestore";
 import "firebase/database";
 
 const config = {
